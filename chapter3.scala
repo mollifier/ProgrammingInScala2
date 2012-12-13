4a768733e05c13fa2067c9558a265cd50eca135e
mollifier <mollifier@gmail.com>
2012-12-14 00:30:55 +0900

    配列を作成

--- chapter3.scala
+++ chapter3.scala
@@ -1,7 +1,17 @@
 
 object Chapter3 {
   def main(args: Array[String]) = {
-    println("Hello, world!")
+    //println("Hello, world!")
+    
+    //val big = new java.math.BigInteger("12345")
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "Hello"
+    greetStrings(1) = ", "
+    greetStrings(2) = "World!"
+    
+    for (i <- 0 to 2) {
+      print(greetStrings(i))
+    }
   }
 }
 
