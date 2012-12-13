44b4ac915b8f59be4a1deb4f2229719dcf49d5ed
mollifier <mollifier@gmail.com>
2012-12-14 00:38:35 +0900

    配列初期化

--- chapter3.scala
+++ chapter3.scala
@@ -4,11 +4,14 @@ object Chapter3 {
     //println("Hello, world!")
     
     //val big = new java.math.BigInteger("12345")
+    /*
     val greetStrings = new Array[String](3)
     greetStrings(0) = "Hello"
     greetStrings(1) = ", "
     greetStrings(2) = "World!"
+    */
     
+    val greetStrings = Array("zero, ", "one, ", "two")
     for (i <- 0 to 2) {
       print(greetStrings(i))
     }
