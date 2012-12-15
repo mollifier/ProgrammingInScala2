3002a90efe3ddb2e57d3b8749a7c72bf946f4552
mollifier <mollifier@gmail.com>
2012-12-15 16:47:09 +0900

    cat format

--- chapter3-6.scala
+++ chapter3-6.scala
@@ -13,14 +13,23 @@ object Chapter3_6 {
       val lines = Source.fromFile(args(0)).getLines().toList
       
       //def widthOfLength(s: String) = s.length.toString.length
-      def widthOfLength(s: String):Int = {
+      def widthOfLength(s: String): Int = {
         s.length.toString.length
       }
       
+      /*
       var maxWidth = 0
       for (line <- lines) {
         maxWidth = maxWidth.max(widthOfLength(line))
       }
+      //*/
+
+      // or
+      def longestLine = lines.reduceLeft(
+        (a, b) => if (a.length > b.length) a else b
+      )
+      val maxWidth = widthOfLength(longestLine)
+
 
       for (line <- lines) {
         val numSpaces = maxWidth - widthOfLength(line)
