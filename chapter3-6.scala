92ac9f17bc29ae0b6b21fcd1e55bf5b0f21322ea
mollifier <mollifier@gmail.com>
2012-12-15 16:43:07 +0900

    cat format

--- chapter3-6.scala
+++ chapter3-6.scala
@@ -4,8 +4,25 @@ object Chapter3_6 {
   def main(args: Array[String]) = {
     
     if (args.length > 0) {
+      /*
       for (line <- Source.fromFile(args(0)).getLines()) {
          println(line.length + " " + line)
+      }
+      //*/
+
+      val lines = Source.fromFile(args(0)).getLines().toList
+      
+      def widthOfLength(s: String) = s.length.toString.length
+      
+      var maxWidth = 0
+      for (line <- lines) {
+        maxWidth = maxWidth.max(widthOfLength(line))
+      }
+
+      for (line <- lines) {
+        val numSpaces = maxWidth - widthOfLength(line)
+        val padding = " " * numSpaces
+        println(padding + line.length + " | " + line)
       }
     } else {
       Console.err.println("Please enter filename")
