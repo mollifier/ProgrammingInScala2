45c44e428de31e8619a5ae775cf62e0034ce63d6
mollifier <mollifier@gmail.com>
2012-12-15 18:49:53 +0900

    シングルトンオブジェクトを実装

--- chapter4/ChecksumAccumulator.scala
+++ chapter4/ChecksumAccumulator.scala
@@ -1,3 +1,26 @@
+import scala.collection.mutable.Map
+
+// シングルトンオブジェクト
+// (コンパニオンクラス)
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+
+  def calculate(s: String): Int = 
+    if (cache.contains(s)) {
+      cache(s)
+    } else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+
+      val cs = acc.checksum()
+
+      cache += (s -> cs)
+      cs
+    }
+
+}
+
 class ChecksumAccumulator {
   private var sum = 0
   def add(b: Byte): Unit = {
@@ -7,5 +30,6 @@ class ChecksumAccumulator {
   def checksum(): Int = {
     return ~(sum & 0xFF) + 1
   }
+  
 }
 
