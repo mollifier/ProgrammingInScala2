b9d80f35a8dd59ce96f6f2b7b9f0859ae7593509
mollifier <mollifier@gmail.com>
2012-12-15 16:32:59 +0900

    cat

--- chapter3-6.scala
+++ chapter3-6.scala
@@ -5,7 +5,7 @@ object Chapter3_6 {
     
     if (args.length > 0) {
       for (line <- Source.fromFile(args(0)).getLines()) {
-         println(line)
+         println(line.length + " " + line)
       }
     } else {
       Console.err.println("Please enter filename")
